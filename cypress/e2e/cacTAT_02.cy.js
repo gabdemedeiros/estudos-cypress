365fb222bfa7b47e31c10bb382e7b830ba5c04f6
joao venceslau <joaovenceslau@MacBook-Pro-de-joao.local>
2025-10-02 16:55:45 -0300

    exercício 02 - Digitando em campos e clicando em elementos

---
+++ cypress/e2e/cacTAT_02.cy.js
@@ -0,0 +1,36 @@
+beforeEach(() => {
+    cy.visit('https://cac-tat-v3.s3.eu-central-1.amazonaws.com/index.html')
+});
+
+describe('Central de Atendimento ao Cliente TAT', () => {
+
+	it('Verifica o mensagem de sucesso', () => {
+    
+    //Verificar mensagem
+    cy.get(".success  > strong").should("have.text", "Mensagem enviada com sucesso.")
+
+  })
+
+  it('Preenche campos obrigatórios e envia o formulário', () => {
+
+    //nome
+    cy.get("#firstName").type("Gabi").click()
+
+    //Sobrenome
+    cy.get("#lastName").type("Medeiros").click()
+
+    //E-mail
+    cy.get("#email").type("testando@email.com").click()
+
+    //Feedback
+    cy.get("#open-text-area").type("testando de novo um texto maior dessa vez").click()
+
+    //Enviar
+    cy.get('button[type="submit"]').click()
+
+		//Verificar mensagem de sucesso
+		cy.get('.success > strong').should('have.text', 'Mensagem enviada com sucesso.')
+
+  });
+
+})
